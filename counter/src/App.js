6735010f89d610a158f3305b8c496cf17237f4b2
Renataclara <renataclarak@gmail.com>
2017-11-22 23:55:09 +0700

    version 0.0.1

---
+++ counter/src/App.js
@@ -0,0 +1,38 @@
+import React, { Component } from 'react';
+import './App.css';
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      count: 0,
+      started: null
+    }
+    this.counterInterval = this.counterInterval.bind(this);
+    this.handleStop = this.handleStop.bind(this);
+  }
+
+  counterInterval () {
+    if (this.state.started === null) {
+      this.setState({ started: setInterval(() => this.setState({ count: this.state.count + 1 }), 100)})
+    }
+  }
+
+  handleStop () {
+    clearInterval(this.state.started)
+    this.setState({started: null})
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <h1 className="App-title">{this.state.count}</h1>
+        <button className="button" onClick={this.counterInterval} >Start</button>
+        <button className="button" onClick={this.handleStop}>Stop</button>
+      </div>
+    );
+  }
+}
+
+
+export default App;
